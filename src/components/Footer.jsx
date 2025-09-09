e93d1dfc16690644401a72625343070fdfe78efb
farhandwk <farhandwiki15@gmail.com>
2025-09-09 16:23:23 +0700

    update backlink event

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,12 +31,12 @@ function Footer({ scrollToSection }) {
     {
       title: "Explore",
       items : [
-        {name: "All Events", link: ""},
+        {name: "All Events", link: "https://event.unteyojourney.com/index.php"},
         {name: "News", link: ""},
         {name: "Creative Media", link: ""},
-        {name: "Events: Sharing Session", link: ""},
-        {name: "Events: Social Education", link: ""},
-        {name: "Events: Movements", link: ""},
+        {name: "Events: Sharing Session", link: "https://event.unteyojourney.com/index.php?kategori=Sharing+Session"},
+        {name: "Events: Social Education", link: "https://event.unteyojourney.com/index.php?kategori=Social+Education"},
+        {name: "Events: Movements", link: "https://event.unteyojourney.com/index.php?kategori=Movement"},
       ]
     }
   ]
